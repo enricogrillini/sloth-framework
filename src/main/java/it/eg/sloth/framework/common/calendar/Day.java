19e62a5128c22fda1eceb4174c1a7e37fe8e920f
egrillini <enrico.grillini@gmail.com>
2023-07-11 07:36:33 +0000

    Allineamento

--- src/main/java/it/eg/sloth/framework/common/calendar/Day.java
+++ src/main/java/it/eg/sloth/framework/common/calendar/Day.java
@@ -4,6 +4,7 @@ import it.eg.sloth.framework.common.base.TimeStampUtil;
 import it.eg.sloth.framework.common.exception.FrameworkException;
 import it.eg.sloth.framework.common.message.Level;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -11,104 +12,109 @@ import java.sql.Timestamp;
 import java.util.*;
 
 @ToString
+@NoArgsConstructor
 public class Day<E, I extends DayInfo> implements Iterable<E> {
 
-  @Getter
-  private Timestamp currentDay;
+    @Getter
+    private Timestamp currentDay;
 
-  @Getter
-  @Setter
-  private boolean locked;
+    @Getter
+    @Setter
+    private boolean locked;
 
-  @Getter
-  @Setter
-  private Level level;
-  
-  @Getter
-  @Setter
-  private String title;
+    @Getter
+    @Setter
+    private Level level;
 
-  @Getter
-  @Setter
-  private String message;
+    @Getter
+    @Setter
+    private String title;
 
-  private List<E> eventList;
-  private Map<String, I> infoMap;
+    @Getter
+    @Setter
+    private String message;
 
-  @Getter
-  @Setter
-  private boolean holiday;
+    private List<E> eventList;
+    private Map<String, I> infoMap;
 
-  public Day(Timestamp day, boolean holiday) {
-    this.currentDay = day;
-    this.holiday = holiday;
-    this.locked = false;
-    this.level = null;
-    this.title = null;
-    this.message = null;
-    this.eventList = new ArrayList<>();
-    this.infoMap = new LinkedHashMap<>();
-  }
+    @Getter
+    @Setter
+    private boolean holiday;
 
-  public void addInfo(I info) {
-    infoMap.put(info.getId(), info);
-  }
+    public Day(Timestamp day, boolean holiday) {
+        this.currentDay = day;
+        this.holiday = holiday;
+        this.locked = false;
+        this.level = null;
+        this.title = null;
+        this.message = null;
+        this.eventList = new ArrayList<>();
+        this.infoMap = new LinkedHashMap<>();
+    }
 
-  public I getInfo(String id) {
-    return infoMap.get(id);
-  }
+    public void addInfo(I info) {
+        infoMap.put(info.getId(), info);
+    }
 
-  public Collection<I> getInfoList() {
-    return Collections.unmodifiableCollection(infoMap.values());
-  }
+    public I getInfo(String id) {
+        return infoMap.get(id);
+    }
 
-  public boolean isMonday() {
-    return TimeStampUtil.getWeekDay(currentDay) == Calendar.MONDAY;
-  }
+    public Collection<I> getInfoList() {
+        return Collections.unmodifiableCollection(infoMap.values());
+    }
 
-  public boolean isTuesday() {
-    return TimeStampUtil.getWeekDay(currentDay) == Calendar.TUESDAY;
-  }
+    public boolean isMonday() {
+        return TimeStampUtil.getWeekDay(currentDay) == Calendar.MONDAY;
+    }
 
-  public boolean isWednesday() {
-    return TimeStampUtil.getWeekDay(currentDay) == Calendar.WEDNESDAY;
-  }
+    public boolean isTuesday() {
+        return TimeStampUtil.getWeekDay(currentDay) == Calendar.TUESDAY;
+    }
 
-  public boolean isThursday() {
-    return TimeStampUtil.getWeekDay(currentDay) == Calendar.THURSDAY;
-  }
+    public boolean isWednesday() {
+        return TimeStampUtil.getWeekDay(currentDay) == Calendar.WEDNESDAY;
+    }
 
-  public boolean isFriday() {
-    return TimeStampUtil.getWeekDay(currentDay) == Calendar.FRIDAY;
-  }
+    public boolean isThursday() {
+        return TimeStampUtil.getWeekDay(currentDay) == Calendar.THURSDAY;
+    }
 
-  public boolean isSaturday() {
-    return TimeStampUtil.getWeekDay(currentDay) == Calendar.SATURDAY;
-  }
+    public boolean isFriday() {
+        return TimeStampUtil.getWeekDay(currentDay) == Calendar.FRIDAY;
+    }
 
-  public boolean isSunday() {
-    return TimeStampUtil.getWeekDay(currentDay) == Calendar.SUNDAY;
-  }
+    public boolean isSaturday() {
+        return TimeStampUtil.getWeekDay(currentDay) == Calendar.SATURDAY;
+    }
 
-  public boolean isToday() throws FrameworkException {
-    return TimeStampUtil.truncSysdate().equals(getCurrentDay());
-  }
+    public boolean isSunday() {
+        return TimeStampUtil.getWeekDay(currentDay) == Calendar.SUNDAY;
+    }
 
-  @Override
-  public Iterator<E> iterator() {
-    return eventList.iterator();
-  }
+    public boolean isToday() throws FrameworkException {
+        return TimeStampUtil.truncSysdate().equals(getCurrentDay());
+    }
 
-  public void addEvent(E event) {
-    eventList.add(event);
-  }
+    @Override
+    public Iterator<E> iterator() {
+        return eventList.iterator();
+    }
 
-  public void removeEvents(E event) {
-    eventList.remove(event);
-  }
+    public int eventCount() {
+        return eventList.size();
+    }
 
-  public void clearEvents() {
-    eventList.clear();
-  }
+    public void addEvent(E event) {
+        eventList.add(event);
+    }
+
+    public void removeEvents(E event) {
+        eventList.remove(event);
+    }
+
+    public void clearEvents() {
+        eventList.clear();
+    }
 
 }
